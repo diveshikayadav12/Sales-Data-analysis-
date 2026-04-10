943984e7e66e9a7ef503f017081d1ad5e2d4d7af
Diveshika Yadav _2410030355_IILMGN <diveshika.yadav.cs28@iilm.edu>
2026-04-10 10:42:18 +0530

    Create Sales_analysis.py for sales data insights
    Added a sales data analysis script with features like monthly revenue trend and product analysis.

---
+++ Sales_analysis.py
@@ -0,0 +1,14 @@
+# Sales Data Analysis
+Analyzed 500 sales records using Python (Pandas, Matplotlib).
+
+## Features
+- Monthly revenue trend
+- Region-wise & category-wise breakdown  
+- Top products analysis
+- 4-chart dashboard
+
+## Tools Used
+Python | Pandas | NumPy | Matplotlib
+
+## Author
+Diveshika Yadav
